9ca67cfc648922cf8f08ea22f477c9109d15f4f2
SantySantos <santysantos533@gmail.com>
2025-01-18 02:03:18 -0500

    WEAPON SYSTEM
    we can switch between weapons with 1 to 4 numbers. Also, we can modify the recoil in each weapon to make it personalize(check scripts). each weapon has a personal amount of bullets, reload time among other qualities, like bullet speed. Some things are missing like the game object that it will fire and check for bugs.

---
+++ Pirate Game Jam/Assets/Scripts/WeaponsScrips/GrenadeScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class GrenadeScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    PlayerMovement pm;
+    //how much should the recoil last for
+    float recoilTime = .3f;
+    float lastRecoil;
+    [SerializeField] float recoilForce = 15f;
+
+    
+    void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        pm = GetComponentInParent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //shoot
+
+            //recoil
+            lastRecoil = recoilTime;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Recoil();
+    }
+    void Recoil()
+    {
+        if (lastRecoil > 0)
+        {
+            //physics calc
+            Vector2 gunToMouse = new Vector2(transform.position.x - pm.mousePos.x, transform.position.y - pm.mousePos.y).normalized;
+            Vector2 recoil = gunToMouse * recoilForce;
+            Vector2 recoilDiff = recoil * lastRecoil / recoilTime;
+            float accelRate = 10f;
+            Vector2 force = recoilDiff * accelRate;
+
+            rb.AddForce(force);
+
+            lastRecoil -= Time.fixedDeltaTime;
+        }
+    }
+}

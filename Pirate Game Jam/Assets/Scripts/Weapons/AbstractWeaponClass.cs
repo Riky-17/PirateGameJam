310e416b26540e3de969cc1f2c068e2d2252da5e
Gordon64 <89490405+Gordon64@users.noreply.github.com>
2025-01-21 02:48:10 -0800

    Revert "Fix merge conflicts"
    This reverts commit 47d931d1a2251d3985ca05a13efa254e5b2c709a.

--- Pirate Game Jam/Assets/Scripts/Weapons/AbstractWeaponClass.cs
+++ Pirate Game Jam/Assets/Scripts/Weapons/AbstractWeaponClass.cs
@@ -8,46 +8,43 @@ public abstract class WeaponSystem : MonoBehaviour
     internal int initialBulletNum;
     public int bulletsNum;
     public float reloadTime;
+    public bool canShoot = true;
     public float bulletSpeed = 10f;
-    internal bool canShoot = true;
-    internal byte weaponIndex;
-    int amountOfCoroutines = 0;
+
     [SerializeField] protected BulletSO bullet;
 
     public abstract void Shoot(Transform muzzle, BulletSO bullet); //shoot method called
 
-    public WeaponDisplay weaponaryText;
+    internal WeaponDisplay weaponaryText;
 
     //timer for reloading weapons when running out of ammo
-
-    public void Reload()
+    public void ManualReload(float loadCooldown)
     {
-
-        CourutineManager.Instance.StartingCoroutine(reloadCourutine());
-    }
-    public IEnumerator reloadCourutine()
-    {
-        amountOfCoroutines++;
-        Debug.Log("Courutine N: " + amountOfCoroutines + " Started");
-        float remainTime = reloadTime;
-        weaponaryText.updateAmmo("Reloading");
-        while (remainTime > 0)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            remainTime -= Time.deltaTime;
-            //Debug.Log(Mathf.Ceil(remainTime).ToString());
-            weaponaryText.loadingInfo(weaponIndex,(Mathf.Ceil(remainTime).ToString()));
+            Debug.Log("ManualReloadStarted");
+            StartCoroutine(ReloadingSpeed(loadCooldown));
+        }
+    }
+    public IEnumerator ReloadingSpeed(float loadCooldown)
+    {
+        weaponaryText.updateAmmo("Reloading");
+        while (loadCooldown > 0)
+        {
+            loadCooldown -= Time.deltaTime;
             yield return null;
         }
-        weaponaryText.disablingLoadingPanels(weaponIndex);
-        weaponaryText.updateAmmo(initialBulletNum.ToString());
-        Debug.Log("Courutine N: " + amountOfCoroutines + " Finished");
         canShoot = true;
         bulletsNum = initialBulletNum;
+        weaponaryText.updateAmmo("Full Ammo!");
+
+        //add in canvas the visual description of this 
 
     }
-    private void Awake()
-    {
 
+    private void Awake() 
+    {       
+        
     }
-
+    
 }
